69a39be882d9710956f0e112b431980aa2f61db2
abidmmp <abidmmp99@gmail.com>
2020-04-22 10:17:00 +0500

    Implement SingletonMeta functionality

--- capcut_auto_basic.py
+++ capcut_auto_basic.py
@@ -344,3 +344,11 @@ def memoize(fn):
             cache[args] = fn(*args)
         return cache[args]
     return wrapper
+
+
+class SingletonMeta(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
