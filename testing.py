bf202b720556919849a2828f6b052219470f902f
abidmmp <abidmmp99@gmail.com>
2020-11-16 11:34:00 +0500

    Optimize chunk_list performance

--- testing.py
+++ testing.py
@@ -495,3 +495,8 @@ class SingletonMeta(type):
         if cls not in cls._instances:
             cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
+
+
+def chunk_list(lst: list, size: int):
+    for i in range(0, len(lst), size):
+        yield lst[i:i + size]
