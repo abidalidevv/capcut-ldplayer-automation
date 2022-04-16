50108b6c702d072dc7b806e0d3d21183026d68df
abidmmp <abidmmp99@gmail.com>
2022-04-16 09:00:00 +0500

    Implement SingletonMeta functionality

--- capcut_auto_basic.py
+++ capcut_auto_basic.py
@@ -453,3 +453,11 @@ def get_env(key: str, default: str = '') -> str:
 
 def count_words(text: str) -> int:
     return len(text.split())
+
+
+class SingletonMeta(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
