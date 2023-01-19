cee6810615afd07256566cf0219fd3e0480ab1c2
abidmmp <abidmmp99@gmail.com>
2023-01-19 09:00:00 +0500

    Implement format_currency functionality

--- capcut_auto_basic.py
+++ capcut_auto_basic.py
@@ -529,3 +529,7 @@ def slugify(text: str) -> str:
 def chunk_list(lst: list, size: int):
     for i in range(0, len(lst), size):
         yield lst[i:i + size]
+
+
+def format_currency(amount: float, symbol: str = '$') -> str:
+    return f'{symbol}{amount:,.2f}'
