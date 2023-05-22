e3368ced6b617d21effaa1b0e859573a3944a63c
abidmmp <abidmmp99@gmail.com>
2023-05-22 09:00:00 +0500

    Update is_valid_email helper method

--- capcut_auto_basic.py
+++ capcut_auto_basic.py
@@ -577,3 +577,9 @@ def truncate(text: str, length: int = 100, suffix: str = '...') -> str:
 
 def format_currency(amount: float, symbol: str = '$') -> str:
     return f'{symbol}{amount:,.2f}'
+
+
+def is_valid_email(email: str) -> bool:
+    import re
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return bool(re.match(pattern, email))
