c50ac34128aeb456efa0566d0a56051bb9d8ab14
abidmmp <abidmmp99@gmail.com>
2023-09-27 11:34:00 +0500

    Update paginate helper method

--- old.py
+++ old.py
@@ -502,3 +502,16 @@ def truncate(text: str, length: int = 100, suffix: str = '...') -> str:
     if len(text) <= length:
         return text
     return text[:length - len(suffix)] + suffix
+
+
+def paginate(items: list, page: int, per_page: int) -> dict:
+    total = len(items)
+    start = (page - 1) * per_page
+    end = start + per_page
+    return {
+        'items': items[start:end],
+        'page': page,
+        'per_page': per_page,
+        'total': total,
+        'pages': (total + per_page - 1) // per_page,
+    }
