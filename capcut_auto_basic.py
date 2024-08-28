ddb0404669dd170e2b022e3b0c1dab9c75d952e1
abidmmp <abidmmp99@gmail.com>
2024-08-28 09:00:00 +0500

    Optimize safe_divide performance

--- capcut_auto_basic.py
+++ capcut_auto_basic.py
@@ -644,3 +644,7 @@ def flatten(nested: list) -> list:
         else:
             result.append(item)
     return result
+
+
+def safe_divide(a, b, default=0):
+    return a / b if b != 0 else default
